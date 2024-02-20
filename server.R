1adfb1b6c7599eaf9d42d180f0d68b77d0f920be
dpiccarreta <159860862+dpiccarreta@users.noreply.github.com>
2024-02-21 00:41:20 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -47,7 +47,7 @@ server <- function(input, output, session) {
   output$resultNumber1 <- renderText({
     if (!is.na(wid1())) {
       return(paste(
-        "-----------Hyper Volume:", input$hv,"+/-",sprintf("%.4f", round(tut1[which(trova_piu_vicino() == medioneautogluon2$hv)[1]], 4)),"\n", "-----------",
+        "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut1[which(trova_piu_vicino() == medioneautogluon2$hv)[1]], 4)),"\n", "-----------",
         "---Kg. CO2: ", round((wid1()/48)^6/100, 2),
         "= ", round(13*(wid1()/48)^6/100/2.380*1000, 2), " Km---"
         
@@ -127,7 +127,7 @@ server <- function(input, output, session) {
  output$resultNumber2 <- renderText({
    if (!is.na(wid2())) {
      return(paste(
-       "-----------Hyper Volume:", input$hv,"+/-",sprintf("%.4f", round(tut2[which(trova_piu_vicino2() == medionefang2$hv)[1]], 4)),"\n","-----------",
+       "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut2[which(trova_piu_vicino2() == medionefang2$hv)[1]], 4)),"\n","-----------",
        "---Kg. CO2: ", round((wid2()/48)^6/100, 2),
        "= ", round(13*(wid2()/48)^6/100/2.380*1000, 2), " Km---"
        
@@ -207,7 +207,7 @@ server <- function(input, output, session) {
   output$resultNumber3 <- renderText({
     if (!is.na(wid3())) {
       return(paste(
-          "-----------Hyper Volume:", input$hv,"+/-",sprintf("%.4f", round(tut3[which(trova_piu_vicino3() == medionebotorch2$hv)[1]], 4)),"\n","-----------",
+          "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut3[which(trova_piu_vicino3() == medionebotorch2$hv)[1]], 4)),"\n","-----------",
         "---Kg. CO2: ", round((wid3()/48)^6/100, 2),
         "= ", round(13*(wid3()/48)^6/100/2.380*1000, 2), " Km---"
         
@@ -236,6 +236,243 @@ server <- function(input, output, session) {
     }
   })
   
+  
+  ######################### XGB ###############
+  wid4 <- reactive({
+    your_dataset_auto <- medioneautogluon_xgb2
+    
+    result_auto <- your_dataset_auto %>%
+      group_by(hv) %>%
+      summarise(y = min(query))
+    
+    result_auto$hv[nrow(result_auto)] <- your_dataset_auto$hv[nrow(your_dataset_auto)] + 0.002
+    
+    get_y_value_auto <- function(x_value, df) {
+      
+      conditions <- list()
+      
+      for (i in 1:(nrow(df) - 1)) {
+        condition <- list(
+          min_value = df$hv[i],
+          max_value = df$hv[i + 1],
+          y_value = df$y[i + 1]
+        )
+        conditions <- c(conditions, list(condition))
+      }
+      
+      for (condition in conditions) {
+        if (x_value >= condition$min_value && x_value < condition$max_value) {
+          return(condition$y_value)
+        }
+      }
+      
+      return(NA)
+    }
+    
+    calculated_width_auto <- get_y_value_auto(input$hv, result_auto)
+    #print(calculated_width_auto)
+    return(((calculated_width_auto  * input$kw_hr * input$kg*input$mix_energetico )^(1/6))*48)
+    print(((calculated_width_auto  * input$kg*input$kw_hr * input$mix_energetico )^(1/6))*48)
+  })
+  trova_piu_vicino4 <- reactive({
+    indice <- which.min(abs(medioneautogluon_xgb2$hv - input$hv))
+    valore_piu_vicino <- medioneautogluon_xgb2$hv[indice]
+    # print(valore_piu_vicino)
+    return(valore_piu_vicino)
+    
+  })
+  
+  output$resultNumber4 <- renderText({
+    if (!is.na(wid4())) {
+      return(paste(
+        "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut1_xgb[which(trova_piu_vicino4() == medioneautogluon_xgb2$hv)[1]], 4)),"\n", "-----------",
+        "---Kg. CO2: ", round((wid4()/48)^6/100, 2),
+        "= ", round(13*(wid1()/48)^6/100/2.380*1000, 2), " Km---"
+        
+      ))
+    }
+  })
+  
+  
+  
+  output$notAvailableText4 <- renderText({
+    if (is.na(wid4())) {
+      shinyjs::runjs("shinyjs.hideImg();")
+      return("Not Available")
+    } else {
+      shinyjs::runjs("shinyjs.showImg();")
+      return(NULL)
+    }
+  })
+  
+  observe({
+    if (!is.na(wid4())) {
+      shinyjs::runjs(sprintf("$('#img4').attr('src', 'fuelauto.jpg');"))
+      shinyjs::runjs(sprintf("$('#img4').width('%s%%');", wid4()))
+      shinyjs::runjs(sprintf("$('#img4').css('margin-top', '%s%%');", 3000/(wid4())-20))
+    } else {
+      shinyjs::runjs(sprintf("$('#img4').attr('src', '');"))  
+    }
+  })
+  
+  
+  
+  
+  wid5 <- reactive({
+    your_dataset_auto <- medionefang_xgb2[1:nrow(medionefang_xgb2),]
+    
+    result_auto <- your_dataset_auto %>%
+      group_by(hv) %>%
+      summarise(y = min(query))
+    
+    result_auto$hv[nrow(result_auto)] <- your_dataset_auto$hv[nrow(your_dataset_auto)] + 0.002
+    
+    get_y_value_auto <- function(x_value, df) {
+      
+      conditions <- list()
+      
+      for (i in 1:(nrow(df) - 1)) {
+        condition <- list(
+          min_value = df$hv[i],
+          max_value = df$hv[i + 1],
+          y_value = df$y[i + 1]
+        )
+        conditions <- c(conditions, list(condition))
+      }
+      
+      for (condition in conditions) {
+        if (x_value >= condition$min_value && x_value < condition$max_value) {
+          return(condition$y_value)
+        }
+      }
+      
+      return(NA)
+    }
+    
+    calculated_width_auto <- get_y_value_auto(input$hv, result_auto)
+    
+    return(((calculated_width_auto  * input$kg*input$kw_hr * input$mix_energetico )^(1/6))*48)
+    print(((calculated_width_auto  * input$kg*input$kw_hr * input$mix_energetico )^(1/6))*48)
+  })
+  trova_piu_vicino5 <- reactive({
+    indice <- which.min(abs(medionefang_xgb2$hv - input$hv))
+    valore_piu_vicino <- medionefang_xgb2$hv[indice]
+    # print(valore_piu_vicino)
+    return(valore_piu_vicino)
+    
+  })
+  
+  output$resultNumber5 <- renderText({
+    if (!is.na(wid5())) {
+      return(paste(
+        "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut2_xgb[which(trova_piu_vicino5() == medionefang_xgb2$hv)[1]], 4)),"\n","-----------",
+        "---Kg. CO2: ", round((wid2()/48)^6/100, 2),
+        "= ", round(13*(wid2()/48)^6/100/2.380*1000, 2), " Km---"
+        
+      ))
+    }
+  })
+  
+  
+  output$notAvailableText5 <- renderText({
+    if (is.na(wid5())) {
+      shinyjs::runjs("shinyjs.hideImg();")
+      return("Not Available")
+    } else {
+      shinyjs::runjs("shinyjs.showImg();")
+      return(NULL)
+    }
+  })
+  
+  observe({
+    if (!is.na(wid5())) {
+      shinyjs::runjs(sprintf("$('#img5').attr('src', 'fuelfang.jpg');"))
+      shinyjs::runjs(sprintf("$('#img5').width('%s%%');", wid5()))
+      shinyjs::runjs(sprintf("$('#img5').css('margin-top', '%s%%');", 3000/(wid5())-20))
+    } else {
+      shinyjs::runjs(sprintf("$('#img5').attr('src', '');"))  # Cambiato per nascondere l'immagine
+    }
+  })
+  
+  
+  
+  #####
+  
+  wid6 <- reactive({
+    your_dataset_auto <- medionebotorch_xgb2[1:nrow(medionebotorch_xgb2),]
+    
+    result_auto <- your_dataset_auto %>%
+      group_by(hv) %>%
+      summarise(y = min(query))
+    
+    result_auto$hv[nrow(result_auto)] <- your_dataset_auto$hv[nrow(your_dataset_auto)] + 0.002
+    
+    get_y_value_auto <- function(x_value, df) {
+      
+      conditions <- list()
+      
+      for (i in 1:(nrow(df) - 1)) {
+        condition <- list(
+          min_value = df$hv[i],
+          max_value = df$hv[i + 1],
+          y_value = df$y[i + 1]
+        )
+        conditions <- c(conditions, list(condition))
+      }
+      
+      for (condition in conditions) {
+        if (x_value >= condition$min_value && x_value < condition$max_value) {
+          return(condition$y_value)
+        }
+      }
+      
+      return(NA)
+    }
+    
+    calculated_width_auto <- get_y_value_auto(input$hv, result_auto)
+    
+    return(((calculated_width_auto  * input$kg*input$kw_hr * input$mix_energetico )^(1/6))*48)
+    print(((calculated_width_auto  * input$kg*input$kw_hr * input$mix_energetico )^(1/6))*48)
+  })
+  trova_piu_vicino6 <- reactive({
+    indice <- which.min(abs(medionebotorch_xgb2$hv - input$hv))
+    valore_piu_vicino <- medionebotorch_xgb2$hv[indice]
+    # print(valore_piu_vicino)
+    return(valore_piu_vicino)
+    
+  })
+  
+  output$resultNumber6 <- renderText({
+    if (!is.na(wid6())) {
+      return(paste(
+        "-----------Hyper Volume:", sprintf("%.3f", round(input$hv, 3)),"+/-",sprintf("%.4f", round(tut3_xgb[which(trova_piu_vicino6() == medionebotorch_xgb2$hv)[1]], 4)),"\n","-----------",
+        "---Kg. CO2: ", round((wid6()/48)^6/100, 2),
+        "= ", round(13*(wid6()/48)^6/100/2.380*1000, 2), " Km---"
+        
+      ))
+    }
+  })
+  
+  
+  output$notAvailableText6 <- renderText({
+    if (is.na(wid6())) {
+      shinyjs::runjs("shinyjs.hideImg();")
+      return("Not Available")
+    } else {
+      shinyjs::runjs("shinyjs.showImg();")
+      return(NULL)
+    }
+  })
+  
+  observe({
+    if (!is.na(wid6())) {
+      shinyjs::runjs(sprintf("$('#img6').attr('src', 'fuelboto.jpg');"))
+      shinyjs::runjs(sprintf("$('#img6').width('%s%%');", wid6()))
+      shinyjs::runjs(sprintf("$('#img6').css('margin-top', '%s%%');", 3000/(wid6())-20))
+    } else {
+      shinyjs::runjs(sprintf("$('#img6').attr('src', '');"))  # Cambiato per nascondere l'immagine
+    }
+  })
   
   
   
--- ui.R
+++ ui.R
@@ -14,14 +14,13 @@ createDataTable <- function() {
   
   datatable(data, options = list(paging = FALSE, searching = FALSE))
 }
-
 ui <- fluidPage(
   useShinyjs(),
   tags$head(
     tags$style(
       HTML(
         "
-        #notAvailableText1, #notAvailableText2, #notAvailableText3 {
+        #notAvailableText1, #notAvailableText2, #notAvailableText3, #notAvailableText4, #notAvailableText5, #notAvailableText6 {
           font-size: 34px;
           text-align: center;
         }
@@ -30,18 +29,18 @@ ui <- fluidPage(
     )
   ),
   
-  titlePanel("CO2 consumption conditioned on a specific Hyper Volume level achieved through HPO of MLP"),
+  titlePanel("CO2 consumption conditioned on a specific Hyper Volume level achieved through HPO"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("hv", "HV", min = 0.72, max = 0.858, value = 0.728),
       sliderInput("kw_hr", "Kwh (hardware specific)", min = 0.1, max = 10, value = 0.5),
       sliderInput("kg", "KgCo2/Kwh", min = 0.01, max = 1, value = 0.53),
       sliderInput("mix_energetico", "Energy Mix", min = 1, max = 100, value = 50),
-     # HTML("<div id='legend10' style='text-align:center;'>0.53 KgCo2/Kwh. Constant Parameter
-      #     <br>Kg.Co2 = 0.53 x Kwh x Energy Mix</div>"),
-     HTML("<div id='legend10' style='text-align:center;'>
+      radioButtons("selectedModel", "Seleziona il modello:",
+                   choices = c("MLP", "XGB"),
+                   selected = "MLP"),
+      HTML("<div id='legend10' style='text-align:center;'>
            <br>Kg.Co2 = Kg.Co2/Kwh x Kwh x h x Energy Mix</div>"),
-     
       div(class = "row text-center",
           div(class = "col-xs-12",
               div(class = "legend-table", createDataTable())
@@ -50,48 +49,93 @@ ui <- fluidPage(
     ),
     mainPanel(
       fluidRow(
-        column(
-          width = 4,
-          align = "center",
-          h3("autogluon-FairBO"),  
-          img(id = "img1", src = "fuelauto.jpg", width = "100%"),
-          br(),
-          textOutput("notAvailableText1"),
-          textOutput("resultNumber1"),
-          shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img1').hide(); }",
-                                 functions = c("hideImg")),
-          shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img1').show(); }",
-                                 functions = c("showImg"))
+        conditionalPanel(
+          condition = "input.selectedModel == 'MLP'",
+          titlePanel("HPO of MLP"),
+          column(
+            width = 4,
+            align = "center",
+            h3("autogluon-FairBO"),  
+            img(id = "img1", src = "fuelauto.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText1"),
+            textOutput("resultNumber1"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img1').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img1').show(); }",
+                                   functions = c("showImg"))
+          ),
+          column(
+            width = 4,
+            align = "center",
+            h3("FanG-HPO"),  
+            img(id = "img2", src = "fuelfang.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText2"),
+            textOutput("resultNumber2"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img2').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img2').show(); }",
+                                   functions = c("showImg"))
+          ),
+          column(
+            width = 4,
+            align = "center",
+            h3("BoTorch-MOMF"),
+            img(id = "img3", src = "fuelboto.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText3"),
+            textOutput("resultNumber3"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img3').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img3').show(); }",
+                                   functions = c("showImg"))
+          )
         ),
-        column(
-          width = 4,
-          align = "center",
-          h3("FanG-HPO"),  
-          img(id = "img2", src = "fuelfang.jpg", width = "100%"),
-          br(),
-          textOutput("notAvailableText2"),
-          textOutput("resultNumber2"),
-          shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img2').hide(); }",
-                                 functions = c("hideImg")),
-          shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img2').show(); }",
-                                 functions = c("showImg"))
-        ),
-        column(
-          width = 4,
-          align = "center",
-          h3("BoTorch-MOMF"),
-          img(id = "img3", src = "fuelboto.jpg", width = "100%"),
-          br(),
-          textOutput("notAvailableText3"),
-          textOutput("resultNumber3"),
-          shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img3').hide(); }",
-                                 functions = c("hideImg")),
-          shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img3').show(); }",
-                                 functions = c("showImg"))
+        conditionalPanel(
+          condition = "input.selectedModel == 'XGB'",
+          titlePanel("HPO of XGB"),
+          column(
+            width = 4,
+            align = "center",
+            h3("autogluon-FairBO"),  
+            img(id = "img4", src = "fuelauto.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText4"),
+            textOutput("resultNumber4"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img4').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img4').show(); }",
+                                   functions = c("showImg"))
+          ),
+          column(
+            width = 4,
+            align = "center",
+            h3("FanG-HPO"),  
+            img(id = "img5", src = "fuelfang.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText5"),
+            textOutput("resultNumber5"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img5').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img5').show(); }",
+                                   functions = c("showImg"))
+          ),
+          column(
+            width = 4,
+            align = "center",
+            h3("BoTorch-MOMF"),
+            img(id = "img6", src = "fuelboto.jpg", width = "100%"),
+            br(),
+            textOutput("notAvailableText6"),
+            textOutput("resultNumber6"),
+            shinyjs::extendShinyjs(text = "shinyjs.hideImg = function() { $('#img6').hide(); }",
+                                   functions = c("hideImg")),
+            shinyjs::extendShinyjs(text = "shinyjs.showImg = function() { $('#img6').show(); }",
+                                   functions = c("showImg"))
+          )
         )
       )
     )
   )
 )
-
-
